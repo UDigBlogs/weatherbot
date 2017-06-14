0bdfa0cc6b26920612741eaff13c3ab641816702
Jeff Crenshaw <jeff.crenshaw@udig.com>
2017-06-14 11:32:41 -0400

    Add project files.

---
+++ WeatherBot/Dialogs/RootDialog.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Connector;
+
+namespace WeatherBot.Dialogs
+{
+    [Serializable]
+    public class RootDialog : IDialog<object>
+    {
+        public Task StartAsync(IDialogContext context)
+        {
+            context.Wait(MessageReceivedAsync);
+
+            return Task.CompletedTask;
+        }
+
+        private async Task MessageReceivedAsync(IDialogContext context, IAwaitable<object> result)
+        {
+            var activity = await result as Activity;
+
+            CurrentConditions conditions = await OpenWeather.GetWeatherAsync(activity.Text);
+ 
+            // return our reply to the user
+            await context.PostAsync(string.Format("Current conditions in {1}: {0}. The temperature is {2}\u00B0 F.", conditions.Weather[0].Main, conditions.CityName, conditions.Main.Temperature));
+
+            context.Wait(MessageReceivedAsync);
+        }
+    }
+}
---
+++ WeatherBot/OpenWeather.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace WeatherBot
+{
+    public class OpenWeather
+    {
+        private const string APIKEY = "[your api key here]";
+
+        public static async Task<CurrentConditions> GetWeatherAsync(string ZipCode)
+        {
+            Uri ServiceURL = new Uri(string.Format("http://api.openweathermap.org/data/2.5/weather?zip={0}&APPID={1}&units=imperial", ZipCode, APIKEY));
+            var client = new HttpClient();
+            HttpResponseMessage response = await client.GetAsync(ServiceURL);
+            response.EnsureSuccessStatusCode();
+            var result = await response.Content.ReadAsStringAsync();
+            CurrentConditions conditions = JsonConvert.DeserializeObject<CurrentConditions>(result);
+
+            return conditions;
+        }
+    }
+}
